094c9153053ab58522eff7f33445394700100db2
Andrew Bondarenko <ent101@outlook.com>
2019-01-09 13:38:34 +0200

    add function

--- src/components/ActiveQuiz/AnswersList/AnswersList.js
+++ src/components/ActiveQuiz/AnswersList/AnswersList.js
@@ -6,8 +6,10 @@ const AnswersList = props => (
   <ul className={classes.AnswersList}>
     {props.answers.map((item, index) => {
       return (
-        <AnswerItem answer={item} key={index} >
-        </AnswerItem>
+        <AnswerItem
+          answer={item} key={index}
+          onAnswerClick={props.onAnswerClick}
+        />
       )
     })}
   </ul>
--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -6,21 +6,34 @@ class Quiz extends Component {
   state = {
     quiz: [
       {
+        question: 'Какого цвета небо?',
+        idx: 1,
+        rightAnswerId: 2,
         answers: [
-          {text: 'Вопрос 1'},
-          {text: 'Вопрос 2'},
-          {text: 'Вопрос 3'},
-          {text: 'Вопрос 4'}
+          {text: 'Черный', id: 1},
+          {text: 'Синий', id: 2},
+          {text: 'Зеленый', id: 3},
+          {text: 'Крастный', id: 4}
         ]
       }
     ]
   }
+
+  onAnswerClickHandler = (answerId) => {
+    console.log(`Answer Id is ${answerId}`)
+  }
+
   render() {
     return (
       <div className={classes.Quiz}>
         <div className={classes.QuizWrapper}>
           <h1>Ответьте на все вопросы</h1>
-          <ActiveQuiz answers={this.state.quiz[0].answers} />
+          <ActiveQuiz 
+            answers={this.state.quiz[0].answers}
+            question={this.state.quiz[0].question}
+            idx={this.state.quiz[0].idx}
+            onAnswerClick={this.onAnswerClickHandler}
+          />
         </div>
       </div>
     )
